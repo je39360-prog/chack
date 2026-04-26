a20f10e0d8a816dd8cde97c53849584fc47c3caf
je39360-prog <je39360@gmail.com>
2026-04-26 16:26:59 +0900

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,3 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = { appId:'com.company.attendance', appName:'Attendance App', webDir:'dist' };
+export default config;
